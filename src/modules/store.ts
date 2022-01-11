cd8e16c41a938ee68ef506744606b8d19636a970
Zasz123 <ygm12345@naver.com>
2022-01-11 21:22:02 +0900

    feat: Add rtk toolkit query settings

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 
+import baseApi from 'services/baseApi';
+
 const makeSore = () => {
   const store = configureStore({
-    reducer: {},
-    middleware: (getDefaultSettings) => getDefaultSettings(),
+    reducer: {
+      [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: (getDefaultSettings) =>
+      getDefaultSettings().concat(baseApi.middleware),
   });
 
   return store;
